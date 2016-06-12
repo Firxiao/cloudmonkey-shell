e687a291de4b49053ef9abfefb784d087b253ac2
Firxiao <Firxiao@FirxiaoMac.lan>
2016-06-13 07:19:47 +0800

    add new script

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# cloudmonkey-shell
+### cloudmonkey-shell
 the shell base on cloudmonkey
 
 
-#功能: 使用shell来对cloudstack进行管理
-#Usage: use shell to manage cloudstack
+###功能: 使用shell来对cloudstack进行管理
+####Usage: use shell to manage cloudstack
 安装配置cloudmonkey 参考:https://cwiki.apache.org/confluence/display/CLOUDSTACK/CloudStack+cloudmonkey+CLI
 How install cloudmonkey :https://cwiki.apache.org/confluence/display/CLOUDSTACK/CloudStack+cloudmonkey+CLI
 
---
+++ destory_all.vm.sh
@@ -0,0 +1,30 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: stop_all_vm.sh
+# 
+#         USAGE: ./stop_all_vm.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Firxiao (), 
+#  ORGANIZATION: 
+#       CREATED: 12/13/15 14:17
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+BIN=`which cloudmonkey`
+
+$BIN list virtualmachines filter=id|grep id|awk '{print $3}' > /tmp/id.temp
+
+while read line
+do
+$BIN destroy virtualmachine expunge=false id=$line
+done < /tmp/id.temp
+
+
